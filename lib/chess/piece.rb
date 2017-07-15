e1b4b3e7b7761a701c0816a5f3be18a4aaa2d551
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-15 00:32:35 -0500

    Passes taking grid as an argument

--- lib/chess/piece.rb
+++ lib/chess/piece.rb
@@ -1,3 +1,7 @@
 class Piece
+	attr_reader :grid
 
+	def initialize(input)
+		@grid = input.fetch(:grid)
+	end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -8,5 +8,13 @@ describe Piece do
 							["_", "_", "_"]]
 			expect { Piece.new(:grid => grid) }.to_not raise_error
 		end
+
+		it "can read the grid" do 
+			grid = [["_", "_", "_"],
+							["_", "_", "_"],
+							["_", "_", "_"]]
+			piece = Piece.new(:grid => grid)
+			expect(piece.grid).to eq grid
+		end
 	end
 end
