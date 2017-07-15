3d4812cef9d64f9bf48617f821a28cd60168a69b
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-15 00:36:37 -0500

    Moves example into grid describe block

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -8,7 +8,9 @@ describe Piece do
 							["_", "_", "_"]]
 			expect { Piece.new(:grid => grid) }.to_not raise_error
 		end
+	end
 
+	describe "#grid" do 
 		it "can read the grid" do 
 			grid = [["_", "_", "_"],
 							["_", "_", "_"],
