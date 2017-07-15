7064821d61740966db06c20c834b07b32119478a
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-15 00:26:07 -0500

    Fails passing grid as argument

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Piece do 
-
+	describe "#initialize" do 
+		it "accepts a grid" do 
+			grid = [["_", "_", "_"],
+							["_", "_", "_"],
+							["_", "_", "_"]]
+			expect { Piece.new(:grid => grid) }.to_not raise_error
+		end
+	end
 end
