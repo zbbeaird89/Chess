c7f05b67c9fdbe78c47466f9d09aec3f81735371
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-14 23:50:57 -0500

    Fails Square default argument

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,5 +1,12 @@
 require "spec_helper"
 
 describe Square do 
-
+	describe "#initialize" do 
+		context "when no argument is passed" do 
+			it "by default sets value = '_'" do 
+				square = Square.new
+				expect(square.value).to eq "_"
+			end
+		end
+	end
 end
