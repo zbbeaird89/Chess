18d518204e1d7ff197ef2fc5c99935b699ffae6e
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-16 14:19:45 -0500

    Passes current square by default is nil

--- lib/chess/piece.rb
+++ lib/chess/piece.rb
@@ -1,5 +1,6 @@
 class Piece
 	attr_reader :grid
+	attr_accessor :current_square
 
 	def initialize(input)
 		@grid = input.fetch(:grid)
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -8,6 +8,14 @@ describe Piece do
 							["_", "_", "_"]]
 			expect { Piece.new(:grid => grid) }.to_not raise_error
 		end
+
+		it "a piece's current square by default is nil" do 
+			grid = [["_", "_", "_"],
+							["_", "_", "_"],
+							["_", "_", "_"]]
+			piece = Piece.new(:grid => grid)
+			expect(piece.current_square).to eq nil
+		end
 	end
 
 	describe "#grid" do 
