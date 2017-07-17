4a89ff6a372d3c9ce6a2eac66393f7729cc88950
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-16 21:56:13 -0500

    Creates default board

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -1,3 +1,13 @@
 class Board
+  attr_reader :grid
 
+  def initialize
+   @grid = default_grid
+  end
+
+  private
+
+    def default_grid
+      Array.new(8) { Array.new(8) { Square.new } }
+    end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe Board do 
+  describe "#initialize" do 
+    it "creates a grid with 8 rows by default" do 
+      board = Board.new
+      expect(board.grid.length).to eq 8
+    end
 
+    it "creates a grid with 8 columns" do 
+      board = Board.new
+      board.grid.each do |row|
+        expect(row.length).to eq 8
+      end
+    end
+  end
 end
