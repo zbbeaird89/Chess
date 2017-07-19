8d0981e29bd9f8bdf6ad65ee211493358375c1ed
Zach Beaird <zbbeaird1989@gmail.com>
2017-07-18 21:39:36 -0500

    Passes initialize with name

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -1,3 +1,5 @@
 class Player
-  
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper"
 
 describe Player do 
-
+  describe "#initialize" do 
+    it "is required to give a name" do 
+      expect { Player.new }.to raise_error(ArgumentError)
+    end
+  end
 end
