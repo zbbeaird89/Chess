cbc3548545d8d2abfd460f0bc4690c231773bdc3
Zach Beaird <zbbeaird1989@gmail.com>
2017-09-15 14:35:13 -0500

    Beginning stages of script

---
+++ script/script.rb
@@ -0,0 +1,39 @@
+require_relative "../lib/chess.rb"
+
+puts "Welcome to Chess!"
+
+puts "Press (1) for 1 Player or press (2) for 2 Players"
+
+number = gets.chomp
+
+player1 = nil
+
+player2 = nil
+
+
+if number.to_i == 2
+  
+  puts "What is Player 1's name?"
+
+  name = gets.chomp
+
+  player1 = Player.new(name, :white)
+
+  puts "What is Player 2's name?"
+
+  name = gets.chomp
+
+  player2 = Player.new(name, :black)
+
+end
+
+
+players = [player1, player2]
+
+Game.new(players).play
+
+
+
+
+
+
