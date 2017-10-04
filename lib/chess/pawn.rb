28e78ed6cb97a43a541236035bfd2a46ec5486c8
Zach Beaird <zbbeaird1989@gmail.com>
2017-10-03 22:00:56 -0500

    Prevents checking for non-existent squares

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -10,38 +10,78 @@ class Pawn < Piece
 
     def white_moves(y, x)
       moves = []
-      forward_one = grid[y - 1][x]
-      forward_two = grid[y - 2][x]
-      left_diag   = grid[y - 1][x - 1]
-      right_diag  = grid[y - 1][x + 1]
 
-      moves << forward_two if first_move && forward_two.value == "_"
+      sqs = squares_on_board(y, x)
 
-      moves << forward_one if forward_one.value == "_"
+      if sqs[:forward_two]
+        if sqs[:first_move] && sqs[:forward_two].value == "_"
+          moves << sqs[:forward_two]
+        end
+      end
 
-      moves << left_diag if left_diag.value.is_a?(Piece) && left_diag.value.color == :black
+      if sqs[:forward_one]
+        if sqs[:forward_one].value == "_"
+          moves << sqs[:forward_one]
+        end
+      end
 
-      moves << right_diag if right_diag.value.is_a?(Piece) && right_diag.value.color == :black
+      if sqs[:left_diag]
+        if sqs[:left_diag].value.is_a?(Piece) && sqs[:left_diag].value.color == :black
+          moves << sqs[:left_diag]
+        end
+      end
+
+      if sqs[:right_diag]
+        if sqs[:right_diag].value.is_a?(Piece) && sqs[:right_diag].value.color == :black
+          moves << sqs[:right_diag]
+        end
+      end
 
       return moves
     end
 
     def black_moves(y, x)
       moves = []
-      forward_one = grid[y + 1][x]
-      forward_two = grid[y + 2][x]
-      left_diag   = grid[y + 1][x - 1]
-      right_diag  = grid[y + 1][x + 1]
 
-      moves << forward_two if first_move && forward_two.value == "_"
+      sqs = squares_on_board(y, x)
 
-      moves << forward_one if forward_one.value == "_"
+      if sqs[:forward_two]
+        if sqs[:first_move] && sqs[:forward_two].value == "_"
+          moves << sqs[:forward_two]
+        end
+      end
 
-      moves << left_diag if left_diag.value.is_a?(Piece) && left_diag.value.color == :white
+      if sqs[:forward_one]
+        if sqs[:forward_one].value == "_"
+          moves << sqs[:forward_one]
+        end
+      end
 
-      moves << right_diag if right_diag.value.is_a?(Piece) && right_diag.value.color == :white
+      if sqs[:left_diag]
+        if sqs[:left_diag].value.is_a?(Piece) && sqs[:left_diag].value.color == :white
+          moves << sqs[:left_diag]
+        end
+      end
+
+      if sqs[:right_diag]
+        if sqs[:right_diag].value.is_a?(Piece) && sqs[:right_diag].value.color == :white
+          moves << sqs[:right_diag]
+        end
+      end
 
       return moves
+    end
+
+    #Eliminates squares checked off of the board
+    def squares_on_board(y, x)
+      directions = {}
+
+      directions[:forward_one] = grid[y + 1][x] unless y + 1 > 7
+      directions[:forward_two] = grid[y + 2][x] unless y + 2 > 7
+      directions[:left_diag]   = grid[y + 1][x - 1] unless y + 1 > 7 || x - 1 < 0
+      directions[:right_diag]  = grid[y + 1][x + 1] unless y + 1 > y || x + 1 > 7
+
+      return directions
     end
 
 end
